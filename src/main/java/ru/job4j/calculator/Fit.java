06324ccb7fd0cfc18ce51ec6f67d1d9bfb23ae2a
jeikhan <evgenii.kapaev@yandex.ru>
2024-08-30 13:22:50 +0300

    6. Идеальный вес [#41584 #5024]

---
+++ src/main/java/ru/job4j/calculator/Fit.java
@@ -0,0 +1,21 @@
+package ru.job4j.calculator;
+
+public class Fit {
+    public static double manWeight(short height) {
+        double result = (height - 100) * 1.15;
+        return result;
+    }
+
+    public static double womanWeight(short height) {
+        double result = (height - 110) * 1.15;
+        return result;
+    }
+
+    public static void main(String[] args) {
+        short height = 184;
+        double man = manWeight(height);
+        double woman = womanWeight(height);
+        System.out.println("Man " + height + " is " + man);
+        System.out.println("Woman " + height + " is " + woman);
+    }
+}
